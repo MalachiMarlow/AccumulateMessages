4266021862df6a7a08ed03f3d9996e8a862d609d
Tusk <Tuskman045@gmail.com>
2025-02-16 15:55:10 -0700

    Add functioning code and makes text green

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,5 +1,12 @@
+'Malachi Marlow
+'Spring 2025
+'RCET2265
+'Accumulate Messages
+'https://github.com/MalachiMarlow/AccumulateMessages.git
+
 
 Imports System
+Imports System.Diagnostics.CodeAnalysis
 
 Module MessageProgram
     Sub Main(args As String())
@@ -9,8 +16,20 @@ Module MessageProgram
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        Console.WriteLine("")
-        Return ""
+        Static message As New Text.StringBuilder
+        'StringBuilder will act like an array builder, basically placing in text that is needed to ensure the function will work.
+        If clear Then
+            message.Clear()
+            ' Will clear the functions message
+        ElseIf newMessage = "" Then
+            message.Append(newMessage)
+            'When blank, the newMessage will display a blank
+        ElseIf newMessage <> "" Then
+            message.AppendLine(newMessage)
+            'When the newMessage is not blank, it will paste whatever stringBuilder has stored. 
+        End If
+
+        Return message.ToString
         'messages
     End Function
 
